c3cfee8f6ff34fa7c21eceaeea5f9b8eccedf751
DonThePhan <donthephan@gmail.com>
2024-12-15 15:20:00 -0500

    added reload state

--- 1.Bare_Bones/App.js
+++ 1.Bare_Bones/App.js
@@ -3,13 +3,15 @@
  *    - uses expo to create a react native project
  *    - --template blank makes sure it's BARE BONES (no TypeScript)
  * 2. PREVIEW app
- *    a)  Via terminal - npm run ios (will need to install EXPO on your mac first)
+ *    a)  Via Mac - 
+ *        1. npm run ios (will need to install EXPO on your mac first) - can also just do "npm start" & choose another simulated device to run on
  *    OR
  *    b)  Via iPhone
  *        1. Install "Expo Go" on iPhone
  *        2. npm start
  *        3. scan QR w/ iPhone camera to automatically open the app on your iPhone in Expo Go
  *    
+ *    *** TO RELOAD, while the simulator is running, in the terminal press "r"
  *
  */
 
